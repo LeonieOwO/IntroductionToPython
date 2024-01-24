63a09c1056eec64baa97367d99d3a28b16b11e24
LeonieOwO <122836295+LeonieOwO@users.noreply.github.com>
2024-01-24 16:54:57 +0000

    Exercise5

--- exercises/exercise_3/main.py
+++ exercises/exercise_3/main.py
@@ -1,23 +1,33 @@
 from vehicle import Vehicle
 import json
+import logging
 
-def load_vehicles_from_json(file_path):
-    with open("vehicles.json", "r") as file:
-        data = json.load(file)
-        vehicles = [Vehicle(**vehicle_data) for vehicle_data in data]
-    return vehicles
+#def load_vehicles_from_json(file_path):
+    #with open("vehicles.json", "r") as file:
+      #  data = json.load(file)
+     #   vehicles = [Vehicle(**vehicle_data) for vehicle_data in data]
+    #return vehicles
 
 
 if __name__ == "__main__":
 
-    json_file_path = 'vehicles.json'  # Setze deinen Dateipfad hier ein
-    vehicles = load_vehicles_from_json(json_file_path)
+    #json_file_path = 'vehicles.json'  # Setze deinen Dateipfad hier ein
+    #vehicles = load_vehicles_from_json(json_file_path)
 
-    print(Vehicle.count)
-    print(Vehicle.power_cons)
+    #print(Vehicle.count)
+    #print(Vehicle.power_cons)
+    logging.basicConfig(format='%(asctime)s :: %(levelname)s :: %(funcName)s :: %(lineno)d \
+        ::\n%(message)s', level = logging.INFO)
+    
 
-    print(Vehicle.count)
+    #air_density = 0
+    #mass = 0
     vehicle = Vehicle(air_density= 1, mass = 3000 )
+    #logging.info(Vehicle.count)
+    logging.info("Vehicle: %s", vehicle)
+    logging.info("Power Consumption: %s", vehicle.power_cons)
+    #logging.info(air_density, mass, vehicle)
+    
     print(vehicle)
     print(vehicle.power_cons)
     
